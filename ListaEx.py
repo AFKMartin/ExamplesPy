a9a6a8d2f735a4c9b9bec475484236e69f92976d
AFKMartin <137821713+AFKMartin@users.noreply.github.com>
2024-07-30 12:30:06 +0200

    ListaEx.py

--- ListaEx.py
+++ ListaEx.py
@@ -1,33 +1,34 @@
-# Crear una lista
+#Ejemplo de uso de una lista
+#Crear una lista
 numeros = [1, 2, 3, 4, 5]
 
-# Acceder a elementos
+#Acceder a elementos
 print("Primer elemento:", numeros[0])  # Salida: 1
 print("Último elemento:", numeros[4])  # Salida: 5
 
-# Modificar elementos
+#Modificar elementos
 numeros[4] = 10
 print("Lista después de modificar el último elemento:", numeros)
 
-# Agregar elementos
+#Agregar elementos
 numeros.append(1)
 numeros.append(22)
 numeros.insert(2, 20)
 print("Lista después de agregar elementos:", numeros)
 
-# Eliminar elementos
+#Eliminar elementos
 numeros.remove(1)
 numeros.remove(1)
 ultimo_elemento = numeros.pop()
 print("Lista después de eliminar elementos:", numeros)
 print("Último elemento eliminado:", ultimo_elemento)
 
-# Iterar sobre la lista
+#Iterar sobre la lista
 print("Elementos de la lista:")
 for numero in numeros:
     print(numero)
 
-# Usar otros métodos
+#Otros métodos
 tamaño = len(numeros)
 numeros_ordenados = sorted(numeros)
 numeros_copia = numeros.copy()
